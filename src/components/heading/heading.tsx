3688bd0646eecd10d02a75c1310a45f2fa8ed56b
wesleykel <wesley.kelly@hotmail.com>
2023-04-16 12:13:03 +0100

    styled components installed

---
+++ src/components/heading/heading.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const Title  = styled.h1`
+
+color:red;
+
+
+
+
+`
+
+
+const Heading = () =>{
+
+
+
+    return(
+
+
+        <Title>Hello world</Title>
+    )
+}
+
+
+export default Heading 
