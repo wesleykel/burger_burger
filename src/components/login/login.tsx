ff13bfe7dca17a00f11359c1aa339a6c0177b675
wesleykel <wesley.kelly@hotmail.com>
2023-04-16 17:16:46 +0100

    next config  updated for styled comp

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,14 +1,18 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
 import styled from "styled-components";
 
-const Login = () => {
-  const { user, error, isLoading } = useUser();
+
 const Wrapper = styled.div`
 
 outline: 1px solid blue;
 
 
 `
+
+
+const Login = () => {
+  const { user, error, isLoading } = useUser();
+
 
   return (
     <Wrapper>
